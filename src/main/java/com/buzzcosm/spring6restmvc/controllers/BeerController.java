64cace3bbf03b3d31bad3f4009fd754f6bd9cdc3
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-23 21:50:29 +0200

    Improve controller

--- src/main/java/com/buzzcosm/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/buzzcosm/spring6restmvc/controllers/BeerController.java
@@ -4,6 +4,7 @@ import com.buzzcosm.spring6restmvc.model.Beer;
 import com.buzzcosm.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,18 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
-    @RequestMapping(value = "/api/v1/beer", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers() {
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id) {
 
         log.debug("Get Beer by Id in controller was called");
 
