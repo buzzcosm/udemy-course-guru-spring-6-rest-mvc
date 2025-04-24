6572c15e08c6f7f0d169bc9a07617d90ed404716
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-24 19:47:37 +0200

    Implement post operation

--- src/main/java/com/buzzcosm/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/com/buzzcosm/spring6restmvc/services/BeerServiceImpl.java
@@ -70,4 +70,24 @@ public class BeerServiceImpl implements BeerService {
         log.debug("Get Beer by Id - in service. Id: {}", id.toString());
         return beerMap.get(id);
     }
+
+    @Override
+    public Beer saveNewBeer(Beer beer) {
+
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .upc(beer.getUpc())
+                .price(beer.getPrice())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        beerMap.put(savedBeer.getId(), savedBeer);
+
+        return savedBeer;
+    }
 }
