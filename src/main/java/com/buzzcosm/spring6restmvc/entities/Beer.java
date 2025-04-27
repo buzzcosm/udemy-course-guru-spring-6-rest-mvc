2719fe681b7ded4bd28fa73acee6c3de256bf09c
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-27 16:45:58 +0200

    Implement entities

---
+++ src/main/java/com/buzzcosm/spring6restmvc/entities/Beer.java
@@ -0,0 +1,36 @@
+package com.buzzcosm.spring6restmvc.entities;
+
+import com.buzzcosm.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Integer version;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
