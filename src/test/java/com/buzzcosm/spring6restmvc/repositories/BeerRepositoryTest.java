de72f3968e71e4f31cfae29db29bf18cc59aae3d
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-06-15 10:34:27 +0200

    add jpa validation for entities

--- src/test/java/com/buzzcosm/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/buzzcosm/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.buzzcosm.spring6restmvc.repositories;
 
 import com.buzzcosm.spring6restmvc.entities.Beer;
+import com.buzzcosm.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                 .beerName("My Beer")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
